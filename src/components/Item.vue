42919d4bafe50809189f02544bc40db5a9d34200
Amyoyzl <1421790940@qq.com>
2019-07-22 21:25:28 +0800

    update item content

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,13 +1,32 @@
 <template>
-    <div id="item">
-        <input type="checkbox" class="done-todo" v-model="item.isChecked" />
-        <span :class="{checked: item.isChecked}">{{ item.content }}</span>
-    </div>
+  <div id="item">
+    <input type="checkbox" class="done-todo" v-model="item.isChecked" />
+    <span
+      :class="{checked: item.isChecked}"
+      @dblclick="editable=true"
+      :contenteditable="editable"
+      ref="info"
+      @keydown="update"
+    >{{ item.content }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "item",
-    props: ['item']
-}
+  name: "item",
+  props: ["item"],
+  data() {
+    return {
+      editable: false
+    };
+  },
+  methods: {
+    update() {
+      if (event.keyCode == 13) {
+        this.editable = false;
+        this.item.content = this.$refs.info.innerHTML;
+      }
+    }
+  }
+};
 </script>
