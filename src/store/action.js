7ee270ecabc05a287e2fb2b8785a00ded43aa918
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-23 18:42:47 +0800

    update the api of updateItem

--- src/store/action.js
+++ src/store/action.js
@@ -3,20 +3,19 @@ import axios from 'axios';
 const baseUrl = "http://localhost:3001/todos"
 
 const actions = {
-    addTodos({ dispatch }, item) {
-        axios.post(baseUrl, item)
-            .then(response => dispatch('getTodos'));
-    },
     getTodos({ commit }) {
         axios.get(baseUrl).then(response => commit('loadItems', response.data));
     },
+    addTodos({ dispatch }, item) {
+        axios.post(baseUrl, item)
+            .then(() => dispatch('getTodos'));
+    },
     updateTodo({ dispatch }, item) {
-        axios.put(`${baseUrl}/${item.id}`).then(response => dispatch('getTodos'));
+        axios.put(`${baseUrl}/${item.id}`, item).then(() => dispatch('getTodos'));
     },
     deleteTodo({ dispatch }, id) {
-        axios.delete(`${baseUrl}/${id}`).then(response => dispatch('getTodos'));
+        axios.delete(`${baseUrl}/${id}`).then(() => dispatch('getTodos'));
     }
-
 }
 
 export default actions;
